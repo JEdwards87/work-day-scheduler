d92291364991023cb2735a8531b6bdc93326a913
JEdwards87 <57024833+JEdwards87@users.noreply.github.com>
2019-11-21 23:12:21 -0600

    Add files via upload
    Added local storage component

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
+//display date and time at top of page
 let dateAndTime = moment().format("dddd, MMMM Do YYYY, h:mm a");
-$('#currentDay').append(dateAndTime)
-let currentHour = moment().hour()
-console.log(dateAndTime)
-console.log(currentHour)
+$('#currentDay').append(dateAndTime);
+let currentHour = moment().hour();
+console.log(dateAndTime);
+console.log(currentHour);
+//save button saves users text input in localStorage
+$(document).ready(function () {
+  const saveButton = $('.saveButton');
+  for (let i = 1; i < 10; i++) {
+    $("#scheduleItem" + i).val(localStorage.getItem("scheduleItem" + i));
+  }
+//Save scheduleItem input to local storage
+  function saveScheduleItem() {      
+    for (let i = 1; i < 10; i++) {
+      localStorage.setItem("scheduleItem" + i, $("#scheduleItem" + i).val());
+    }
+  };
+  $('.saveButton').on("click", saveScheduleItem);
+});
 
